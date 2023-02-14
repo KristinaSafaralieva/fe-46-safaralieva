c842d159467b36c35e8dc86427aff84b819caabf
KristinaSafaralieva <kristinatichankova@gmail.com>
2023-02-14 20:45:14 +0400

    Updated optimisation_maket

--- axit-project/script/sidebar_menu.js
+++ axit-project/script/sidebar_menu.js
@@ -1,9 +1,7 @@
 const sidebar = document.querySelector(".sidebar_menu");
-const openBtn = document.querySelector('.sidebar_open-btn');
-const closeBtn = document.querySelector('.sidebar_close-btn');
+const sidebarBtn = document.querySelector('.sidebar_btn');
 
 const onBurgerClick = () => {
     sidebar.classList.toggle("sidebar_menu_active");
-    openBtn.classList.toggle('sidebar_button-active');
-    closeBtn.classList.toggle('sidebar_button-active');
+    sidebarBtn.classList.toggle('sidebar_btn-close');
 }
--- axit-project/script/slider.js
+++ axit-project/script/slider.js
@@ -1,20 +1,9 @@
-const sliderCards = document.querySelector('.reviews_cards')
+const sliderCardsElement = document.querySelector('.reviews_cards')
 const sliderButtons = document.querySelectorAll('.slider_controls_button');
 
-const onLeftArrowClick = (el) => {
+const onDotClick = (el) => {
     sliderButtons.forEach(btn => btn.classList.remove('slider_controls_button-active'));
     el.classList.add('slider_controls_button-active');
-    sliderCards.setAttribute('style', `transform: translateX(0%)`);
-}
-
-const onCenterArrowClick = (el) => {
-    sliderButtons.forEach(btn => btn.classList.remove('slider_controls_button-active'));
-    el.classList.add('slider_controls_button-active');
-    sliderCards.setAttribute('style', `transform: translateX(-33.33%)`);
-}
-
-const onRightArrowClick = (el) => {
-    sliderButtons.forEach(btn => btn.classList.remove('slider_controls_button-active'));
-    el.classList.add('slider_controls_button-active');
-    sliderCards.setAttribute('style', `transform: translateX(-66.66%)`);
+    const sliderPosition = el.getAttribute('data-slider-position') || '0';
+    sliderCardsElement.setAttribute('style', `transform: translateX(${sliderPosition})`);
 }
